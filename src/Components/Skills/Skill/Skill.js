07e8c70640e5f2e16d2926052bca6c6c14464042
KauhareniaNastassia <kavgorenya_a@tut.by>
2023-03-16 23:04:33 +0300

    add adaptive styles to skills

--- src/Components/Skills/Skill/Skill.js
+++ src/Components/Skills/Skill/Skill.js
@@ -3,15 +3,23 @@ import style from './Skill.module.scss'
 import Tilt from 'react-tilt'
 
 function Skill(props) {
-    return (
-        <Tilt className="Tilt">
+    return (/*<Tilt className="Tilt">*/
+        <div className={style.wrapper}>
+
             <div className={style.skill}>
                 <div className={style.icon}
-                     style={props.style}></div>
-                <h3>{props.title}</h3>
+                     style={props.style}>
+                </div>
+
+                <div className={style.cardBack}>
+                    <h3>{props.title}</h3> {/*card back*/}
+                </div>
+
             </div>
-        </Tilt>
-    );
+
+        </div>
+
+        /*</Tilt>*/);
 }
 
 export default Skill;
